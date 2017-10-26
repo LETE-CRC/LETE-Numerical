c6352110148ad337d925304c383ef395074b3b0a
filipimfs <filipimfs@gmail.com>
2017-10-26 17:24:29 -0200

    Functions were fixed

--- MovMesh/MovingMesh.py
+++ MovMesh/MovingMesh.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 11 17:23:30 2015 by helio
-Modified on Tue Oct 17 18:50:33 2017 by filipi
-
+Created on Thu Jun 11 2015 by helio
+Modified on Tue Oct 24 2017 by filipi
 """
+
 import numpy as np
 import re
 from scipy.sparse.linalg import bicg # bicgstab
@@ -12,6 +12,9 @@ import shutil
 import os
 from timeit import default_timer as timer
 
+#==============================================================================
+# --------------------- INPUTS E SELEÇÕES NECESSÁRIAS ------------------------#
+#==============================================================================
 
 ## -- Propriedades
 gamma = 0.1 # RIGIDEZ DA MALHA
@@ -20,7 +23,7 @@ LaplacianScheme = 'linear'
 
 
 ###############################################################################
-# --------------- IO - LEITURA E ESCRITA DE ARQUIVOS ------------------------ #
+# --------------- ---------------- FUNÇÕES ---------------------------------- #
 ###############################################################################
     
 def read_file(arquivo): # funcao que le arquivos de malha do OpenFOAM
@@ -44,12 +47,16 @@ def read_file(arquivo): # funcao que le arquivos de malha do OpenFOAM
             
         return (arquivo, quantidade)
 
+#=============================================================================#
+
 def read_scalarlist(path): # func que le arquivos de escalares (owner/neighbour)
     with open(path, 'r') as infile:
         arquivo = infile.read()
         arquivo = arquivo[arquivo.find("(")+1:arquivo.find(")")]
         lista = re.findall(r'\s*(\d*\.\d+|\d+)', arquivo)
     return lista
+
+#=============================================================================#
 
 def read_bcs_mesh(): # funcao que le condicoes de contorno da malha do OpenFOAM
     with open('./constant/polyMesh/boundary', 'r') as infile: # abre o arquivo a ser lido
@@ -61,6 +68,8 @@ def read_bcs_mesh(): # funcao que le condicoes de contorno da malha do OpenFOAM
         startface = re.findall(r'startFace(\w+)', arquivo) # encontra id primeira face na lista de faces
         nfaces = np.asarray(nfaces);nfaces = nfaces.astype(np.int)
     return (namebcs, tipobc, nfaces, startface)
+
+#=============================================================================#
 
 def write_OF(varName,solution): # func que escreve resultados
     '''
@@ -100,9 +109,7 @@ def write_OF(varName,solution): # func que escreve resultados
     with open(pathFinal, 'w') as f:
         f.write(data_chopped)
 
-###############################################################################
-# --------------- CALCULOS RELATIVOS AS FACES DA MALHA ---------------------- #
-###############################################################################
+#=============================================================================#
 
 def calc_mesh_faces():
     areaFace = np.zeros(len(faces))
@@ -125,9 +132,7 @@ def calc_mesh_faces():
         
     return (areaFace, areaFaceV, cFace)
 
-###############################################################################
-# --------------- CALCULOS RELATIVOS AOS VOLUMES DA MALHA ------------------- #
-###############################################################################
+#=============================================================================#
 
 def calc_mesh_vol():
     Nvolumes = np.max(owner)+1 #+1 contagem inicia em 0...
@@ -154,17 +159,17 @@ def calc_mesh_vol():
         vol[i] = vol[i]/3
     return (Nvolumes, cVol, vol)
 
-###############################################################################
-# --------------- CALCULOS DAS MATRIZES DO SISTEMA LINEAR ------------------- #
-###############################################################################
+#=============================================================================#
 
-def assembly():
+def solucao():
     A = np.zeros([Nvolumes,Nvolumes]);Su = np.zeros(Nvolumes) ## termo FONTE
     Sp = np.zeros(Nvolumes); SuBC = np.zeros(Nfaces); SpBC = np.zeros(Nfaces)
     d = np.zeros(Nneighbour);dVet = np.zeros((Nneighbour,3))
     dPfV = np.zeros((Nfaces,3)); dPf = np.zeros((Nfaces))
     dDeltaV = np.zeros((Nneighbour,3));dDelta = np.zeros(Nneighbour)
     aNf = np.zeros(Nneighbour)
+    
+    DeslocamentoBC[:]=np.nan
 
     for i in range(Nneighbour): # loop faces internas
         o = owner[i] ; n = neighbour[i]
@@ -226,6 +231,7 @@ def assembly():
             for k in range(int(startFace[j]), int(startFace[j])+int(nFaces[j])):
                 SuBC[k] = (areaFace[k] * gamma / dPf[k]) * valor
                 SpBC[k] = -areaFace[k] * gamma / dPf[k]
+
         
     for i in range(Nfaces):
         o = owner[i] # para cada face, o é o valor da lista owner (n do vol de controle)
@@ -236,10 +242,76 @@ def assembly():
     for i in range(len(A)):
         A[i,i] = -np.sum(A[i])-Sp[i] # aP da soma dos vizinhos
         
-    return (A, Su)
+    return (A, Su, OutputFacesC, DeslocamentoBC)
+
+#=============================================================================#
+def interpolator_corrector():
+    dCent = np.zeros(Nneighbour)
+    dVolN = np.zeros(Nneighbour)
+    dVolO = np.zeros(Nneighbour)
+    OutputFaces = np.zeros([Nowner,3])
+    DeslocamentoA = np.zeros([Npoints,3])
+    DeslocamentoV = np.zeros([Npoints,3])
+    DeslocamentoBC = np.empty([Nfaces,3])
+    DeslocamentoBC[:]=np.nan
+    
+    for i in range(Nneighbour): # loop faces internas
+            o = owner[i] ; n = neighbour[i]
+            dCent[(i)] = np.linalg.norm(cVol[n] - cVol[o]) # módulo da distância entre centros vol N e O
+            dVolN[(i)] = np.linalg.norm(cVol[n] - cFace[i] ) # módulo da distância entre centros vol N e face
+            dVolO[(i)] = np.linalg.norm(cFace[i] - cVol[o]) # módulo da distância entre centros vol face e O
+            OutputFaces[i,0] = (Output[o]*dVolO[i]+Output[n]*dVolN[i])/dCent[i] #interpolando o resultado para as faces
+    
+    with open('./0/T', 'r') as infile:
+        arquivo = infile.read()
+        arquivo = ''.join(arquivo.split()) # retira todos espacos em branco
+    
+    for i in NameBCs:
+            my_regex1 = re.escape(i) + r"\{type(\w+)\}*"
+            my_regex2 = re.escape(i) + r"\{type\w+\;valueuniform(\w+)\}*"
+            TipobcDic = re.findall(my_regex1, arquivo)
+            TipobcDic = str(TipobcDic).strip('[\']')
+            valor = re.findall(my_regex2, arquivo, re.MULTILINE)
+            valor = str(valor).strip('[\']')
+            try:
+                valor = float(valor)
+            except:
+                pass
+                
+            print re.escape(i),'--->', TipobcDic, '--->',valor # Mostra no terminal as condicoes de contorno
+            
+            if TipobcDic == 'empty' or TipobcDic == 'symmetryPlane':
+                j = NameBCs.index(i)
+                for k in range(int(startFace[j]), int(startFace[j])+int(nFaces[j])):
+                    o = owner[k]
+                    OutputFaces[k,0] = Output[o]             
+                    
+            if TipobcDic == 'fixedValue':
+                j = NameBCs.index(i)
+                for k in range(int(startFace[j]), int(startFace[j])+int(nFaces[j])):
+                    o = owner[k]
+                    OutputFaces[k,0] = valor
+                    DeslocamentoBC[k,0] = valor
+    
+    for i in range(Npoints): #interpolando os resultados
+        k=0
+        for j in range(Nfaces):
+            if i in faces[j]:
+                DeslocamentoA[i,0]+=OutputFaces[j,0]
+                k+=1
+        DeslocamentoV[i,0]=DeslocamentoA[i,0]/k
+        
+    for i in range(Nfaces): #corrigindo os resultados das BC
+        if not np.isnan(DeslocamentoBC[i,0]):
+            DeslocamentoV[int(faces[i][0]),0] = DeslocamentoBC[i,0]
+            DeslocamentoV[int(faces[i][1]),0] = DeslocamentoBC[i,0]
+            DeslocamentoV[int(faces[i][2]),0] = DeslocamentoBC[i,0]
+            DeslocamentoV[int(faces[i][3]),0] = DeslocamentoBC[i,0]
+        
+    return (DeslocamentoV)
 
 ###############################################################################
-# ----------------------------- INICIO DO SOLVER ---------------------------- #
+# ------------------------------ SOLVER --------------------------------------#
 ###############################################################################
 
 intro = '\n============================================='
@@ -251,144 +323,100 @@ print (intro)
 
 StartTimeTot = timer() # contabilizador do tempo
 
-### Leitura arquivos da malha
-## -- Computa nos da malha
+### LEITURA DA MALHA
+## -- Computa nós da malha
 points, Npoints = read_file('./constant/polyMesh/points')
 points = np.asarray(points);points = points.astype(np.float)
+#=============================================================================#
 ## -- Computa faces da malha
 faces, Nfaces = read_file('./constant/polyMesh/faces')
 
 for i in range(len(faces)): # necessario as vezes...
     faces[i] = np.array(faces[i])
     faces[i] = faces[i].astype(np.int)
-
+#=============================================================================#
 ## -- Computa volumes donos das faces na malha
 owner = read_scalarlist('./constant/polyMesh/owner')
 owner = np.asarray(owner);owner = owner.astype(np.int)
 Nowner = len(owner)
+#=============================================================================#
 ## -- Computa volumes vizinhos das faces na malha
 neighbour = read_scalarlist('./constant/polyMesh/neighbour')
 neighbour = np.asarray(neighbour);neighbour = neighbour.astype(np.int)
 Nneighbour = len(neighbour)
-
+#=============================================================================#
 timeFiles = timer()
-
-print(' Calculando areas e centros das areas da malha')
+#=============================================================================#
 ## -- Computa a area (modulo e vetor) de cada face e os centros
+print(' Calculando áreas e centros das faces da malha')
 areaFace, areaFaceV, cFace = calc_mesh_faces()
-
+#tempo gasto
 timeA = timer()
 timeArea = timer() - timeFiles
 print("done: %f s " %timeArea)
 print(' Calculando volumes e centros dos volumes da malha')
+#=============================================================================#
 ## -- Computa o numero de volumes da malha e as coords centroides dos vols
 Nvolumes, cVol, Vol = calc_mesh_vol()
-
+#tempo gasto
 timeVol = timer() - timeA
 print("done: %f s " %timeVol)
-
 timeMesh = timer()
-
-print ('----------- Condicoes de contorno -----------\n')
-
+#=============================================================================#
+#=============================================================================#
+### CONDIÇÕES DE CONTORNO
+print ('----------- Condições de contorno -----------\n')
 ## -- Computa infos de condicoes de contorno
 NameBCs, TipoBCs, nFaces, startFace = read_bcs_mesh()
-
+#=============================================================================#
 ## -- Computa a matriz dos coeficientes e vetor lado direito
-A, Su = assembly()
-
-## -- Solucao do sistema linear
+A, Su, OutputFacesC, DeslocamentoBC = solucao()
+#=============================================================================#
+#=============================================================================#
+### SOLUÇÃO DA EQUAÇÃO DE DIFUSÃO
+## -- Solução do sistema linear
 StartTimeSisLin = timer()
 Solucao = bicg(A, Su)
-#Solucao = bicgstab(A, Su)
+#Solucao = bicgstab(A, Su) #alternativa
 Output = Solucao[0]
-
-#MERDA AQUI
-dCent = np.zeros(Nneighbour)
-dVolN = np.zeros(Nneighbour)
-dVolO = np.zeros(Nneighbour)
-OutputFaces = np.zeros([Nowner,3])
-DeslocamentoA = np.zeros([Npoints,3])
-DeslocamentoV = np.zeros([Npoints,3])
-DeslocamentoBC = np.empty([Nfaces,3])
-DeslocamentoBC[:]=np.nan
-
-for i in range(Nneighbour): # loop faces internas
-        o = owner[i] ; n = neighbour[i]
-        dCent[(i)] = np.linalg.norm(cVol[n] - cVol[o]) # módulo da distância entre centros vol N e O
-        dVolN[(i)] = np.linalg.norm(cVol[n] - cFace[i] ) # módulo da distância entre centros vol N e face
-        dVolO[(i)] = np.linalg.norm(cFace[i] - cVol[o]) # módulo da distância entre centros vol face e O
-        OutputFaces[i,0] = (Output[o]*dVolO[i]+Output[n]*dVolN[i])/dCent[i] #interpolando o resultado para as faces
-
-with open('./0/T', 'r') as infile:
-    arquivo = infile.read()
-    arquivo = ''.join(arquivo.split()) # retira todos espacos em branco
-
-for i in NameBCs:
-        my_regex1 = re.escape(i) + r"\{type(\w+)\}*"
-        my_regex2 = re.escape(i) + r"\{type\w+\;valueuniform(\w+)\}*"
-        TipobcDic = re.findall(my_regex1, arquivo)
-        TipobcDic = str(TipobcDic).strip('[\']')
-        valor = re.findall(my_regex2, arquivo, re.MULTILINE)
-        valor = str(valor).strip('[\']')
-        try:
-            valor = float(valor)
-        except:
-            pass
-            
-        print re.escape(i),'--->', TipobcDic, '--->',valor # Mostra no terminal as condicoes de contorno
-        
-        if TipobcDic == 'empty' or TipobcDic == 'symmetryPlane':
-            j = NameBCs.index(i)
-            for k in range(int(startFace[j]), int(startFace[j])+int(nFaces[j])):
-                o = owner[k]
-                OutputFaces[k,0] = Output[o]             
-                
-        if TipobcDic == 'fixedValue':
-            j = NameBCs.index(i)
-            for k in range(int(startFace[j]), int(startFace[j])+int(nFaces[j])):
-                o = owner[k]
-                OutputFaces[k,0] = valor
-                DeslocamentoBC[k,0] = valor
-
-for i in range(Npoints): #interpolando os resultados
-    k=0
-    for j in range(Nfaces):
-        if i in faces[j]:
-            DeslocamentoA[i,0]+=OutputFaces[j,0]
-            k+=1
-    DeslocamentoV[i,0]=DeslocamentoA[i,0]/k
-    
-for i in range(Nfaces): #corrigindo os resultados das BC
-    if not np.isnan(DeslocamentoBC[i,0]):
-        DeslocamentoV[int(faces[i][0]),0] = DeslocamentoBC[i,0]
-        DeslocamentoV[int(faces[i][1]),0] = DeslocamentoBC[i,0]
-        DeslocamentoV[int(faces[i][2]),0] = DeslocamentoBC[i,0]
-        DeslocamentoV[int(faces[i][3]),0] = DeslocamentoBC[i,0]
-#FIM DA MERDA AQUI
-
+#tempo gasto
 TimeFinal = timer()
 WallTimeSisLin = TimeFinal - StartTimeSisLin
 WallTimeTot = TimeFinal - StartTimeTot
 WallTimeFiles = timeFiles - StartTimeTot
 WallTimeMesh = timeMesh - timeFiles
 WallTimeAssembly = StartTimeSisLin - timeMesh
-
+#=============================================================================#
+## -- Interpolação e correção do resultado
+ResultadoFinal=interpolator_corrector()
+#=============================================================================#
+## -- Impressão no terminal dos resultados para conferência
 print ('Volumes: %d' %Nvolumes)
 final1 = '\n============================================='
-final1 += '\n--------------- Solução em X ----------------\n\n'
+final1 += '\n----------------- Solução  ------------------\n\n'
 final2 = '\n============================================='
-print (final1 + str(DeslocamentoV) + final2)
-
-#np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-
+print (final1 + str(ResultadoFinal) + final2)
+#=============================================================================#
+## - Impressão do tempo total gasto para a solução final
 print ('Tempo para leitura dos arquivos: %f segundos' % WallTimeFiles)
-print ('Tempo para calculos da malha: %f segundos' % WallTimeMesh)
-print ('Tempo para calculos das matrizes: %f segundos' % WallTimeAssembly)
+print ('Tempo para cálculos da malha: %f segundos' % WallTimeMesh)
+print ('Tempo para cálculos das matrizes: %f segundos' % WallTimeAssembly)
 print ('Tempo para solucao sistema linear: %f segundos' % WallTimeSisLin)
 print ('Tempo total: %f segundos' % WallTimeTot)
-print ('=============================================')
-
+#=============================================================================#
+## -- Escrita do arquivo de saída
 #write_OF('T',T)
+#=============================================================================#
+## -- Mensagem de aviso de fim de código
+fim = '\n============================================='
+fim += '\n============================================='
+fim += '\n------------- Código finalizado -------------'
+fim += '\n============================================='
+fim += '\n============================================='
+print (fim)
 
-print ('ACABOOOOU, É TETRAAAAAAAAAAAAAAA')
+#fim do código
+#=============================================================================#
+#=============================================================================#
+#=============================================================================#
+#=============================================================================#
